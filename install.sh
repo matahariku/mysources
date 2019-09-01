adb3142d90eebcb6f50d8cccf5ae2a64aa9e15af
matahariku <54765115+matahariku@users.noreply.github.com>
2019-09-01 17:15:44 +0200

    install.sh: to be launched at first start on destination host

---
+++ install.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+#
+# GOAL
+# 
+# - once downloaded (git/http/scp)
+#   curl http://github.com/matahariku/mysources/install.sh
+#   to local user home directory,
+
+usage {
+cat << EOT
+./install.sh # will install below <HOME>/mysources
+EOT
+} 
+
+[ -f $HOME/install.sh ] || {
+ usage
+}
